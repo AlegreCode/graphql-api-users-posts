8bac77d720bcbd0be3c0266bd123a91327d02265
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-12-03 18:33:42 -0300

    Generamos los resolvers con los nuevos modelos

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,20 +2,6 @@
 
 package model
 
-type Post struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
-	User  *User  `json:"user"`
-}
-
-type User struct {
-	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Lastname string  `json:"lastname"`
-	Posts    []*Post `json:"posts"`
-}
-
 type InputPost struct {
 	Title string `json:"title"`
 	Text  string `json:"text"`
