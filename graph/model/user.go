332aa86943b23c3ce8f07bf013db8fcb5e89d9ea
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-12-16 19:46:31 -0300

    Al modelo user.go en el campo Posts añadimos la etiqueta de gorm para que al borrar el registro padre, se borren todos los registros hijos, con OnDelete=CASCADE

--- graph/model/user.go
+++ graph/model/user.go
@@ -4,5 +4,5 @@ type User struct {
 	ID       int     `json:"id"`
 	Name     string  `json:"name"`
 	Lastname string  `json:"lastname"`
-	Posts    []*Post `json:"posts"`
+	Posts    []*Post `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"posts"`
 }
